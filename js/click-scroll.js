49b070e75ba8e8d2fbc84b634ae8f4de1f194e15
harikrishnags <harikrisz7060@gmail.com>
2024-02-23 15:07:36 +0530

    fifth commit

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -6,7 +6,7 @@ var sectionArray = [1, 2, 3, 4, 5, 6, 7];
 $.each(sectionArray, function(index, value){
           
      $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 84;
+         var offsetSection = $('#' + 'section_' + value).offset();
          var docScroll = $(document).scrollTop();
          var docScroll1 = docScroll + 1;
          
